3b0b8d109743d60d5d435ed2437cac2c1715b875
zzw <2849983729@qq.com>
2021-05-22 22:55:34 +0800

    hello.java

--- src/test/hello.java
+++ src/test/hello.java
@@ -4,5 +4,6 @@ public class hello {
     public static void main(String[] args){
         System.out.println("helloword");
         System.out.println("test远程");
+        System.out.println("helloword222");
     }
 }
